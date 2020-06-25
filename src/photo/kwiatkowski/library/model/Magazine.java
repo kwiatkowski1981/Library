f1758fe05ab27858a1248a19595a6f5fe106989a
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-06-25 22:02:56 +0200

    feat: added constructor qnd getters + setters

--- src/photo/kwiatkowski/library/model/Magazine.java
+++ src/photo/kwiatkowski/library/model/Magazine.java
@@ -1,4 +1,40 @@
 package photo.kwiatkowski.library.model;
 
-public class Magazine {
+public class Magazine extends Publication {
+
+    private int month;
+    private int day;
+    private String language;
+
+
+    public Magazine(int month, int day, String language) {
+        this.month = month;
+        this.day = day;
+        this.language = language;
+    }
+
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
