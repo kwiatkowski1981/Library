fda0968e08cace2ebbf8f1d29a4389712acf0810
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-06-25 22:46:30 +0200

    feat: created a new method 'add and create' a new Magazine object

--- src/photo/kwiatkowski/library/model/Library.java
+++ src/photo/kwiatkowski/library/model/Library.java
@@ -3,8 +3,11 @@ package photo.kwiatkowski.library.model;
 public class Library {
 
     private static final int MAX_BOOKS = 1000;
+    private static final int MAX_MAGAZINE = 1000;
     private Book[] books = new Book[MAX_BOOKS];
+    private Magazine[] magazines = new Magazine[MAX_MAGAZINE];
     private int booksNumber = 0;
+    private int magazinesNumber = 0;
 
     public void addBook(Book book) {
         if (booksNumber < MAX_BOOKS) {
@@ -24,4 +27,25 @@ public class Library {
             books[i].printInfo();
         }
     }
+
+    public void addMagazine(Magazine magazine) {
+        if (magazinesNumber < MAX_MAGAZINE) {
+            magazines[magazinesNumber] = magazine;
+            magazinesNumber++;
+        }else{
+            System.out.println("the maximum number of Magazines You can add has been reached");
+        }
+    }
+
+    public void printMagazines() {
+        if (magazinesNumber == 0) {
+            System.out.println("no magazine has been added yet");
+
+        }
+        for (int i = 0; i < magazinesNumber; i++) {
+            magazines[i].printInfo();
+        }
+    }
+
+
 }
